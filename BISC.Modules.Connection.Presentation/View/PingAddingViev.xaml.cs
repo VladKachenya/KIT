5692b1720c8d3d82df4def7ba1f8b0761f9cc4ac
Влад Каченя <v.s.kachenya@gmail.com>
2018-07-13 22:17:48 +0300

    Доработана модель Connections. В частности добавлен класс описывающий IP. Отредактировано View Connection. (View пока что не работает с моделью)

---
+++ BISC.Modules.Connection.Presentation/View/PingAddingViev.xaml.cs
@@ -0,0 +1,30 @@
+﻿using BISC.Modules.Connection.Presentation.Interfaces.Ping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BISC.Modules.Connection.Presentation.View
+{
+    /// <summary>
+    /// Логика взаимодействия для PingsViev.xaml
+    /// </summary>
+    public partial class PingAddingViev : UserControl
+    {
+        public PingAddingViev(IPingAddingViewModel pingAddingViewModel)
+        {
+            InitializeComponent();
+            DataContext = pingAddingViewModel;         
+        }
+    }
+}
