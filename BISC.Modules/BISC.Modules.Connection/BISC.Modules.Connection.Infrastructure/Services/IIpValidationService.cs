af80882b86e2e4672fe965b6f3c7639ff4978d51
Влад Каченя <v.s.kachenya@gmail.com>
2018-07-14 14:58:03 +0300

    Добавлен сервис для PING

---
+++ BISC.Modules/BISC.Modules.Connection/BISC.Modules.Connection.Infrastructure/Services/IIpValidationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BISC.Modules.Connection.Infrastructure.Services
+{
+    public interface IIpValidationService
+    {
+        bool IsSimplifiedIpAddress(string Address);
+
+        bool IsExactFormIpAddress(string Address);
+    }
+}
---
+++ BISC.Modules/BISC.Modules.Connection/BISC.Modules.Connection.Infrastructure/Services/IPingService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BISC.Modules.Connection.Infrastructure.Services
+{
+    public interface IPingService
+    {
+        Task<bool> GetPing(string ip);
+    }
+}
