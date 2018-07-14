4c34c654646f61ec14cc3227e5c4eb8692e63746
Влад Каченя <v.s.kachenya@gmail.com>
2018-07-14 14:57:32 +0300

    Добавлен сервис для PING

--- BISC.Modules.Connection.Model/Services/IpValidationServic.cs
+++ BISC.Modules.Connection.Model/Services/IpValidationServic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BISC.Modules.Connection.Infrastructure.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace BISC.Modules.Connection.Model.Services
 {
-    static public class StaticValidatorIp
+    public class IpValidationServic : IIpValidationService
     {
         static  private System.Text.RegularExpressions.Regex IpMatch =
         new System.Text.RegularExpressions.Regex(@"\b(?:\d{1,3}\.){3}\d{1,3}\b");
@@ -14,12 +15,12 @@ namespace BISC.Modules.Connection.Model.Services
         static private System.Text.RegularExpressions.Regex IpMatchAdvanse =
        new System.Text.RegularExpressions.Regex(@"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
 
-        static public bool IsSimplifiedIpAddress(string Address)
+        public bool IsSimplifiedIpAddress(string Address)
         {
             return IpMatch.IsMatch(Address);
         }
 
-        static public bool IsExactFormIpAddress(string Address)
+        public bool IsExactFormIpAddress(string Address)
         {
             return IpMatchAdvanse.IsMatch(Address);
         }
---
+++ BISC.Modules.Connection.Model/Services/PingService.cs
@@ -0,0 +1,68 @@
+﻿using BISC.Modules.Connection.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BISC.Modules.Connection.Model.Services
+{
+    public class PingService : IPingService
+    {
+        #region private filds
+        private AutoResetEvent waiter = new AutoResetEvent(false);
+        #endregion
+
+        #region Citor
+        public PingService()
+        {
+
+        }
+        #endregion
+
+        #region Implementation of IPingService
+        public async Task<bool> GetPing(string ip)
+        {
+
+            SemaphoreSlim semaphoreSlim = new SemaphoreSlim(0);
+            var isPing = false;
+            Ping pinger = new Ping();
+            pinger.PingCompleted += (o, e) =>
+            {
+                if (e.Cancelled)
+                {
+                    ((AutoResetEvent)e.UserState).Set();
+                }
+                if (e.Error != null)
+                {
+                    ((AutoResetEvent)e.UserState).Set();
+                }
+
+                PingReply reply = e.Reply;
+                ((AutoResetEvent)e.UserState).Set();
+                isPing = reply.Status == IPStatus.Success;
+                semaphoreSlim.Release();
+            };
+            PingOptions pingOptions = new PingOptions(128, true);
+            int timeout = 1000;
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            try
+            {
+                pinger.SendAsync(ip, timeout, buffer, pingOptions, waiter);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            await semaphoreSlim.WaitAsync();
+            return isPing;
+        }
+        #endregion
+
+    }
+}
