cfea8d8e5c304c60e5683606dfe60494a822b229
Влад Каченя <v.s.kachenya@gmail.com>
2019-04-10 16:37:07 +0300

    Добавленна обработка ошибок для fcda

--- BISC.Modules/BISC.Modules.DataSets/BISC.Modules.DataSets.Presentation/Factorys/FcdaViewModelFactory.cs
+++ BISC.Modules/BISC.Modules.DataSets/BISC.Modules.DataSets.Presentation/Factorys/FcdaViewModelFactory.cs
@@ -10,6 +10,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BISC.Infrastructure.Global.Logging;
+using BISC.Infrastructure.Global.Services;
 using BISC.Modules.DataSets.Infrastructure.Services;
 using BISC.Modules.DataSets.Infrastructure.ViewModels.Base;
 using BISC.Modules.DataSets.Presentation.HelperEntites;
@@ -21,17 +23,42 @@ namespace BISC.Modules.DataSets.Presentation.Factorys
     {
         private readonly IInjectionContainer _injectionContainer;
         private readonly IFcdaInfoService _fcdaInfoService;
+        private readonly ILoggingService _loggingService;
 
-        public FcdaViewModelFactory(IInjectionContainer injectionContainer, IFcdaInfoService fcdaInfoService)
+        public FcdaViewModelFactory(IInjectionContainer injectionContainer, IFcdaInfoService fcdaInfoService, ILoggingService loggingService)
         {
             _injectionContainer = injectionContainer;
             _fcdaInfoService = fcdaInfoService;
+            _loggingService = loggingService;
         }
         public ObservableCollection<IFcdaViewModel> CreateFcdaViewModelCollection(IDevice device, IDataSet model, IWeigher parentWeiger)
         {
             ObservableCollection<IFcdaViewModel> result = new ObservableCollection<IFcdaViewModel>();
             foreach (IFcda element in model.ChildModelElements)
-                result.Add(CreateFcdaViewModelElement(device, element, parentWeiger));
+            {
+                IFcdaViewModel fcda;
+                try
+                {
+                    fcda = CreateFcdaViewModelElement(device, element, parentWeiger);
+                }
+                catch (Exception e)
+                {
+                    string res;
+                    if (string.IsNullOrEmpty(element.DaName))
+                    {
+                        res = $"{element.LdInst}/{element.Prefix + element.LnClass + element.LnInst}.{element.DoName}";
+                    }
+                    else
+                    {
+                        res =
+                            $"{element.LdInst}/{element.Prefix + element.LnClass + element.LnInst}.{element.DoName}.{element.DaName}";
+                    }
+                    _loggingService.LogMessage($"Ошибка при формировании fcda {res}", SeverityEnum.Critical);
+                    continue;
+                }
+                result.Add(fcda);
+            }
+
             return result;
         }
 
